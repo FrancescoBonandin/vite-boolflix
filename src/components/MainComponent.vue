a3aee59b3ee9e7d08ace5dd2792cee9b53f9c492
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-07-19 21:47:50 +0200

    EDIT MS3 Completata

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -14,7 +14,26 @@ export default {
         }
     },
     methods: {
+        getStarVote(element){
 
+           const stars= parseInt(element.vote_average / 2)
+           if(stars >=0 && stars <=1){
+            stars=1
+           }
+           else if(stars>4 && stars<=5){
+            stars=5
+           }
+
+
+           return stars
+        }
+    },
+    computed:{
+        imgPathPrefix(){
+           let imgUrl='https://image.tmdb.org/t/p/w500'
+            return imgUrl
+        },
+     
     }
 }
 </script>
@@ -22,33 +41,56 @@ export default {
 <template>
     <main>
         <div class="container">
+
             <h2 v-if="store.searchResults.movies.length > 0">Movies</h2>
+
             <div class="row">
+
                 <div v-for="(singleMovie,i) in store.searchResults.movies" :key="i" class="col-auto">
-                    <p>titolo: {{ singleMovie.title }} </p>
-                    <p>titolo originale:{{ singleMovie.original_title }} </p>
-                    
-                    <p>lingua:
-                        <span v-if="singleMovie.original_language!=''">
-                            <CountryFlag :country="singleMovie.original_language=='en'? 'gb':singleMovie.original_language " size="small"/>
-                        </span>
 
-                        <span v-else>
+                    <div class="card">
+
+                        <div class="card-img-top">
+                            <img :src="`${imgPathPrefix}${singleMovie.poster_path}`" alt="">
+                        </div>
+    
+                        <p>titolo: {{ singleMovie.title }} </p>
+
+                        <p>titolo originale:{{ singleMovie.original_title }} </p>
+                        
+                        <p>lingua:
+                            <span v-if="singleMovie.original_language!=''">
+                                <CountryFlag :country="singleMovie.original_language=='en'? 'gb':singleMovie.original_language " size="small"/>
+                            </span>
+    
+                            <span v-else>
+                                
+                                {{ singleMovie.original_language }}
+    
+                            </span>
+                        </p>
+
+                        <p>voto: 
+                            <font-awesome-icon v-for="x in getStarVote(singleMovie)" icon="fa-solid fa-star"/>
+                            <font-awesome-icon v-for="y in (5 - getStarVote(singleMovie))" icon="fa-regular fa-star"/>
                             
-                            {{ singleMovie.original_language }}
+                        </p>
+                        
+                    </div>
 
-                        </span>
-                    </p>
-                    <p>voto: {{ parseFloat(singleMovie.vote_average).toFixed(1) }} </p>
-                    
                 </div>
+
             </div>
 
             <h2 v-if="store.searchResults.tvSeries.length > 0">TV series</h2>
 
-            <div class="row">
+            <div class="row row-cols-4">
+                
                 <div v-for="(singleSerie,i) in store.searchResults.tvSeries" :key="i" class="col-auto">
+
+                    
                     <p>titolo: {{ singleSerie.name }} </p>
+
                     <p>titolo originale:{{ singleSerie.original_name }} </p>
                     
                     <p>lingua:
@@ -62,17 +104,30 @@ export default {
 
                         </span>
                     </p>
+
                     <p>voto: {{ parseFloat(singleSerie.vote_average).toFixed(1) }} </p>
                     
                 </div>
+
             </div>
+
         </div>
+
     </main>
 </template>
 
 <style lang="scss" scoped>
-.col-auto{
+
+.card{
     border:solid 1px red;
+    .card-img-top{
+        width: 50%;
+        border: solid 1px blue;
+        img{
+            width: 100%;
+    
+        }
+    }
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,18 @@ import App from './App.vue'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
+
+
+/* add icons to the library */
+library.add(faStarSolid,faStarRegular)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
